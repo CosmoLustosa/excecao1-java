dca6e36b14fd46f0000cac7237b2d7fffba6257e
Cosmo Lustosa <cosmolustosa2@gmail.com>
2019-05-06 23:25:16 -0300

    Alteracao na classe reserva

--- src/model/entities/Reserva.java
+++ src/model/entities/Reserva.java
@@ -1,5 +1,58 @@
 package model.entities;
 
-public class Reserva {
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
+public class Reserva {
+	private Integer numeroQuarto;
+	private Date entrada;
+	private Date saida;
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	
+	public Reserva(Integer numeroQuarto, Date entrada, Date saida) {
+		this.numeroQuarto = numeroQuarto;
+		this.entrada = entrada;
+		this.saida = saida;
+	}
+
+	public Integer getNumeroQuarto() {
+		return numeroQuarto;
+	}
+
+	public void setNumeroQuarto(Integer numeroQuarto) {
+		this.numeroQuarto = numeroQuarto;
+	}
+
+	public Date getEntrada() {
+		return entrada;
+	}
+
+	public Date getSaida() {
+		return saida;
+	}
+
+	public long duracao() {
+		long diff = saida.getTime() - entrada.getTime();
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+		
+	}
+	
+	public void atualizaData(Date entrada, Date saida) {
+		this.entrada = entrada;
+		this.saida = saida;		
+	}
+	
+	@Override
+	public String toString() {
+		return "Quarto "
+				+ numeroQuarto
+				+ ", Entrada: "
+				+ sdf.format(entrada)
+				+ ", Saida: "
+				+ sdf.format(saida)
+				+ ", "
+				+ duracao()
+				+ "noites";
+	}
 }
